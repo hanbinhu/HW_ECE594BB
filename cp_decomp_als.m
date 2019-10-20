51f47bf9f26607a699c4b26da83f770995477f43
Hanbin Hu <hanbinhu2016@gmail.com>
2019-10-19 17:09:53 -0700

    Set optional parameter for CP decomposition

--- cp_decomp_als.m
+++ cp_decomp_als.m
@@ -1,4 +1,4 @@
-function [Xhat, lambda, K, K_init] = cp_decomp_als(X, rank)
+function [Xhat,lambda,K,K_init,converge] = cp_decomp_als(X, rank, varargin)
 %CP_DECOMP_ALS performs CP decomposition on the given tensor.
 %   This function uses alternating least square (ALS) method to decompose
 %   tensor 'X' into two parts: weighting parameter lambda for rank-1
@@ -6,32 +6,74 @@ function [Xhat, lambda, K, K_init] = cp_decomp_als(X, rank)
 % Parameter:
 % - 'X' is the tensor for CP decomposition.
 % - 'rank' is the rank for CP decomposition.
+% - 'maxiters' (optional, default: 100) is the maximum iteration for the CP 
+%   decomposition.
+% - 'difftol' (optional, default: 1e-4) is the tolerance value for the
+%   change of normalized error norm.
+% - 'abstol' (optional, default: 1e-2) is the tolerance value for the
+%   normalized error norm.
+% - 'verbose' (optional, default: false) is the switch for covergence
+%   information printout.
 % Output:
 % - 'Xhat' is the approximated tensor of X using CP decomposition.
 % - 'lambda' is the weighting parameter for all rank-1 vectors.
 % - 'K' is a cell array containing all the rank-1 vectors.
 % - 'K_init' is a cell array containing all initial guess of the rank-1
 %   vectors.
-    maxiters = 1000;
-    epstol = 1e-4;
+% - 'converge' gives if the CP decomposition coverges.
 
+    % set defaults for optional inputs
+    parser = inputParser;
+    parser.addParameter('maxiters', 100,...
+        @(x) isscalar(x) && x>0 && floor(x)==x);
+    parser.addParameter('difftol', 1e-4,...
+        @(x) isscalar(x) && x>0);
+    parser.addParameter('abstol', 1e-2,...
+        @(x) isscalar(x) && x>0);
+    parser.addParameter('verbose', false,...
+        @(x) isscalar(x) && islogical(x));
+    parser.parse(varargin{:});
+    maxiters = parser.Results.maxiters;
+    difftol = parser.Results.difftol;
+    abstol = parser.Results.abstol;
+    verbose = parser.Results.verbose;
+    
+    % Get number of ways
     nway = ndims(X);
     
     % Generate initial guess
+    if verbose
+        fprintf('Generating initial guess ... ');
+    end
     K_init = cell(1, nway);
     for i = 1:nway
         K_init{i} = normc(rand(size(X,i), rank)*2-1);
     end
+    if verbose
+        fprintf('Done\n');
+    end
     
     % Precompute KtK
+    if verbose
+        fprintf('Precomputing ... ');
+    end
     KtK = zeros(nway, rank, rank);
     for i = 1:nway
         KtK(i,:,:)=K_init{i}'*K_init{i};
+    end
+    if verbose
+        fprintf('Done\n');
     end
     
     K = K_init;
     lambda = ones(rank,1);
     % ALS steps
+    ipX = sqrt(innerprod(X, X));
+    perr = 0;
+    converge = false;
+    if verbose
+        fprintf('Start ALS iterations ... ');
+    end
     for i = 1:maxiters
         % Iterater for matrices
         for j = 1:nway
@@ -52,10 +94,28 @@ function [Xhat, lambda, K, K_init] = cp_decomp_als(X, rank)
         % Check Convergence
         diff = X-Xhat;
         err = sqrt(innerprod(diff, diff));
-        if err < epstol
-            break
+        nerr = err / ipX;
+        err_diff = abs(nerr-perr);
+        if verbose
+            fprintf(' Iter %2d: norm_error = %.4e, diff_error = %.4e\n',...
+                i, nerr, err_diff);
         end
-        fprintf(' Iter %2d: err = %.4e\n', i, err);
+        if err_diff < difftol && nerr < abstol
+            converge = true;
+            break;
+        end
+        perr = nerr;
     end
+    if verbose
+        if ~converge
+            warning(['The CP decomposition is not converged under the '...
+                sprintf('maximum %d iterations with the ', maxiters)...
+                sprintf('given tolerance of %.1e (abs)/%.1e (diff).\n',...
+                abstol, difftol)]);
+        else
+            fprintf('Coverged.\n');
+        end
+    end
+    
 end
 
--- testP1.m
+++ testP1.m
@@ -1,20 +1,36 @@
 clear; clc; close all;
 seed = 10;
+
 dim = [3,4,2];
 rank = 4;
-rank_cp = 4;
 lambda_range = 10;
 
+verbose = true;
+
+% Parameters for CP decomposition
+rank_cp = 4;
+maxiters_cp = 100;
+abstol_cp = 1e-2;
+difftol_cp = 1e-4;
+
 % Set random seed
-rng(seed);
+%rng(seed);
 
 % Generate the low rank tensor
 [X, lambda_X, K_X] = generate_low_rank_tensor(dim, rank, lambda_range);
-[Xhat_CP, lambda_X_CP, K_X_CP, K_init_X_CP] = cp_decomp_als(X, rank_cp);
-
 [Y, lambda_Y, K_Y] = generate_low_rank_tensor(dim, rank, lambda_range);
-[Yhat_CP, lambda_Y_CP, K_Y_CP, K_init_Y_CP] = cp_decomp_als(Y, rank_cp);
 
+% Get CP decomposition
+[Xhat_CP, lambda_X_CP, K_X_CP, K_init_X_CP, converge_X_CP] = ...
+    cp_decomp_als(X, rank_cp,...
+        'maxiters', maxiters_cp, 'verbose', verbose,...
+        'abstol', abstol_cp, 'difftol', difftol_cp);
+[Yhat_CP, lambda_Y_CP, K_Y_CP, K_init_Y_CP, converge_Y_CP] = ...
+    cp_decomp_als(Y, rank_cp,...
+        'maxiters', maxiters_cp, 'verbose', verbose,...
+        'abstol', abstol_cp, 'difftol', difftol_cp);    
+
+% Inner production computation
 ip_acc = innerprod(X,Y);
 ip_app = innerprod(Xhat_CP,Yhat_CP);
 
