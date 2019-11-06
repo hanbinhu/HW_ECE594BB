7b848cd20378098bdd9f60d14405359016ea6e6d
Hanbin Hu <hanbinhu2016@gmail.com>
2019-11-06 15:10:54 -0800

    Add inner product for Tensor Train decomposition with other decomposed tensors

--- ip_TT_CP_TN.m
+++ ip_TT_CP_TN.m
@@ -1,4 +1,4 @@
-function ip = ip_TT_CP_TN(TT_X,TT_rank_X,l_Y,K_Y)
+function ip = ip_TT_CP_TN(TT_X,l_Y,K_Y)
 %ip_TT_CP_TN computes the inner product of two decomposed tensors.
 %   This function takes one Tensor Train decomposition of one tensor 'X'
 %   and the CP decomposition of another tensor 'Y', and returns the inner
@@ -6,12 +6,35 @@ function ip = ip_TT_CP_TN(TT_X,TT_rank_X,l_Y,K_Y)
 % Parameter:
 % - 'TT_X' is the a cell array containing all the tensors for X from Tensor
 %   Train decomposition.
-% - 'TT_rank_X' is a vector containing all the tensor ranks for X from
-%   Tensor Train decomposition.
 % - 'l_Y' is the weighting parameter 'lambda' for Y from CP decomposition.
 % - 'K_Y' is the matrices cell array 'K' for Y from CP decomposition.
 % Output:
 % - 'ip' is the resulting inner product of X and Y.
-ip = 0;
+
+    % check rank input
+    if numel(TT_X) ~= numel(K_Y)
+        error('Input tensors are not the same size.');
+    end
+    
+    nway = numel(K_Y);
+    
+    % Compute the inner product
+    middle_layer = cell(1,nway);
+    for i = 1:nway-1
+        middle_layer{i} = permute(ttt(TT_X{i},tensor(K_Y{i}),2,1),[3,1,2]);
+    end
+    middle_layer{1} = squeeze(middle_layer{1});
+    middle_layer{nway} = ...
+        permute(ttt(TT_X{nway},tensor(K_Y{nway}),2,1),[2,1]);
+    
+    ip = 0;
+    for i = 1:numel(l_Y)
+        w = double(middle_layer{1}(i,:))';
+        for j = 2:nway-1
+            w = w*double(squeeze(middle_layer{j}(i,:,:)));
+        end
+        w = w*double(middle_layer{nway}(i,:));
+        ip = ip+l_Y(i)*w;
+    end
 end
 
--- ip_TT_Tucker_TN.m
+++ ip_TT_Tucker_TN.m
@@ -1,4 +1,4 @@
-function ip = ip_TT_Tucker_TN(TT_X,TT_rank_X,G_Y,K_Y)
+function ip = ip_TT_Tucker_TN(TT_X,G_Y,K_Y)
 %ip_TT_Tucker_TN computes the inner product of two decomposed tensors.
 %   This function takes one Tensor Train decomposition of one tensor 'X'
 %   and the Tucker decomposition of another tensor 'Y', and returns the
@@ -6,12 +6,24 @@ function ip = ip_TT_Tucker_TN(TT_X,TT_rank_X,G_Y,K_Y)
 % Parameter:
 % - 'TT_X' is the a cell array containing all the tensors for X from Tensor
 %   Train decomposition.
-% - 'TT_rank_X' is a vector containing all the tensor ranks for X from
-%   Tensor Train decomposition.
 % - 'G_Y' is the core tensor 'G' for Y from Tucker decomposition.
 % - 'K_Y' is the matrices cell array 'K' for Y from Tucker decomposition.
 % Output:
 % - 'ip' is the resulting inner product of X and Y.
-ip = 0;
+    
+    % check rank input
+    if numel(TT_X) ~= numel(K_Y)
+        error('Input tensors are not the same size.');
+    end
+    
+    nway = numel(K_Y);
+    
+    % Compute the inner product
+    XX=tensor(1);
+    for i = 1:nway-1
+        temp = permute(ttt(TT_X{i},tensor(K_Y{i}),2,1),[1,3,2]);
+        XX=ttt(XX,temp,ndims(XX),1);
+    end
+    XX=squeeze(ttt(XX,ttt(TT_X{nway},tensor(K_Y{nway}),2,1),ndims(XX),1));
+    ip=innerprod(XX,G_Y);
 end
-
